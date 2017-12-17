ce0b3f28b1c542f8d728f32f3e8380b10d24ca18
frederick-wang <hwoam@outlook.com>
2017-12-18 05:08:53 +0800

    没有trim的bug修正

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ ep.after(`province_fetched`, provinceNames.length, data => {
   start()
 })
 for (let provName of provinceNames) {
+  try {
+    fs.mkdirSync(`output/${provName}/`)
+  } catch (error) {
+  }
   let provUrl = `http://daxue.netbig.com/search/?prov=${urlencode(provName)}`
   superagent.get(provUrl)
     .end((err, res) => {
@@ -85,15 +89,15 @@ function start () {
             console.log(`${school}(${id})没有老师！`)
             console.log(`开始创建${school}(${id})的空文件……`)
             console.log(`创建文件 output/${province}/${school}.csv`)
-            fs.writeFileSync(`output/${province.province}/${school}.csv`, '')
-            console.log(`${province.province}/${school} 空文件创建完毕！`)
-            console.log('-----开始10分钟的等待-----')
+            fs.writeFileSync(`output/${province}/${school}.csv`, '')
+            console.log(`${province}/${school} 空文件创建完毕！`)
+            console.log('-----开始1分钟的等待-----')
             setTimeout(() => {
               callback(null, {
                 school: school,
                 teachers: teachers
               })
-            }, 1000 * 60 * 10)
+            }, 1000 * 60)
             return
           }
           let tmpA = $('.page_one').find('a')
@@ -105,19 +109,19 @@ function start () {
               console.log(`${school}(${id})全部老师抓取完成！`)
               console.log(`开始保存${school}(${id})老师数据……`)
               console.log(`创建文件 output/${province}/${school}.csv`)
-              fs.writeFileSync(`output/${province.province}/${school}.csv`, '')
-              console.log(`正在写入数据 output/${province.province}/${school}.csv ……`)
+              fs.writeFileSync(`output/${province}/${school}.csv`, '')
+              console.log(`正在写入数据 output/${province}/${school}.csv ……`)
               for (let teacherData of teachers) {
-                fs.appendFileSync(`output/${province.province}/${school}.csv`, `${teacherData.name},${teacherData.school},${teacherData.college}\r\n`)
+                fs.appendFileSync(`output/${province}/${school}.csv`, `${teacherData.name},${teacherData.school},${teacherData.college}\r\n`)
               }
-              console.log(`${province.province}/${school} 全部老师数据保存完毕！`)
-              console.log('-----开始10分钟的等待-----')
+              console.log(`${province}/${school} 全部老师数据保存完毕！`)
+              console.log('-----开始1分钟的等待-----')
               setTimeout(() => {
                 callback(null, {
                   school: school,
                   teachers: teachers
                 })
-              }, 1000 * 60 * 10)
+              }, 1000 * 60)
               return
             }
             // 处理多页
@@ -151,9 +155,9 @@ function start () {
                         }
                         let $ = cheerio.load(res.text)
                         let p3 = $('.b_techer1_con_l1').find('p')
-                        let name = p3.eq(0).text().trim().replace('姓名：', '') || 'N/A'
-                        let school = p3.eq(1).text().trim().replace('所在大学： ', '') || 'N/A'
-                        let college = p3.eq(2).text().trim().replace('所在院系： ', '') || 'N/A'
+                        let name = p3.eq(0).text().trim().replace('姓名：', '').trim() || 'N/A'
+                        let school = p3.eq(1).text().trim().replace('所在大学：', '').trim() || 'N/A'
+                        let college = p3.eq(2).text().trim().replace('所在院系：', '').trim() || 'N/A'
                         ep.emit(`teachers_in_a_page_fetched_${id}_${item.page}`, {
                           name: name,
                           school: school,
@@ -173,19 +177,19 @@ function start () {
               console.log(`${school}(${id})全部老师抓取完成！`)
               console.log(`开始保存${school}(${id})老师数据……`)
               console.log(`创建文件 output/${province}/${school}.csv`)
-              fs.writeFileSync(`output/${province.province}/${school}.csv`, '')
-              console.log(`正在写入数据 output/${province.province}/${school}.csv ……`)
+              fs.writeFileSync(`output/${province}/${school}.csv`, '')
+              console.log(`正在写入数据 output/${province}/${school}.csv ……`)
               for (let teacherData of teachers) {
-                fs.appendFileSync(`output/${province.province}/${school}.csv`, `${teacherData.name},${teacherData.school},${teacherData.college}\r\n`)
+                fs.appendFileSync(`output/${province}/${school}.csv`, `${teacherData.name},${teacherData.school},${teacherData.college}\r\n`)
               }
-              console.log(`${province.province}/${school} 全部老师数据保存完毕！`)
-              console.log('-----开始10分钟的等待-----')
+              console.log(`${province}/${school} 全部老师数据保存完毕！`)
+              console.log('-----开始1分钟的等待-----')
               setTimeout(() => {
                 callback(null, {
                   school: school,
                   teachers: teachers
                 })
-              }, 1000 * 60 * 10)
+              }, 1000 * 60)
             })
           })
           dlNodes.find('a').each((index, element) => {
@@ -197,9 +201,9 @@ function start () {
                 }
                 let $ = cheerio.load(res.text)
                 let p3 = $('.b_techer1_con_l1').find('p')
-                let name = p3.eq(0).text().trim().replace('姓名：', '') || 'N/A'
-                let school = p3.eq(1).text().trim().replace('所在大学： ', '') || 'N/A'
-                let college = p3.eq(2).text().trim().replace('所在院系： ', '') || 'N/A'
+                let name = p3.eq(0).text().trim().replace('姓名：', '').trim() || 'N/A'
+                let school = p3.eq(1).text().trim().replace('所在大学：', '').trim() || 'N/A'
+                let college = p3.eq(2).text().trim().replace('所在院系：', '').trim() || 'N/A'
                 ep.emit(`teachers_in_a_page_fetched_${id}_1`, {
                   name: name,
                   school: school,
